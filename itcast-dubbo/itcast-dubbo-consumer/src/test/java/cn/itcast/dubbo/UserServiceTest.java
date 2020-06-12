37d61f1bb00ce6acad4344cfe5518ca48ea8fed4
Bruce Lee <1135365441@qq.com>
2020-06-12 20:45:34 +0800

    好客租房dubbo

---
+++ itcast-dubbo/itcast-dubbo-consumer/src/test/java/cn/itcast/dubbo/UserServiceTest.java
@@ -0,0 +1,27 @@
+package cn.itcast.dubbo;
+
+import cn.itcast.dubbo.pojo.User;
+import cn.itcast.dubbo.service.UserService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserServiceTest {
+
+    @Reference(version = "1.0.0")
+    private UserService userService;
+
+    @Test
+    public void testQueryAll() {
+        List<User> users = this.userService.queryAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+}
